6cb015b57d78ad6c8aa74e5ebc76d4913efaabce
bart-van-hees <bartvanhees@student.hu.nl>
2022-06-16 15:10:19 +0200

    testen of code werkt voor 3x3 functie

---
+++ sudoku algoritme/Test.py
@@ -0,0 +1,53 @@
+test_sudoku_empty = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+gok = 8
+rij = 0
+colom = 4
+def grid(rij,colom,gok):
+    if rij <= 2:
+        if colom <= 2:
+            for item in range(3):
+                for x in range(3):
+                    if (test_sudoku_empty[item][x]) == gok:
+                        return False
+        elif colom <= 5:
+            for item in range(3):
+                for x in range(3, 6):
+                    if (test_sudoku_empty[item][x]) == gok:
+                        return False
+        elif colom <= 8:
+            for item in range(3):
+                for x in range(6, 9):
+                    if (test_sudoku_empty[item][x]) == gok:
+                        return False
+    # elif rij <= 5:
+    #     if colom <=
+    #
+    #
+    #
+    # elif rij <= 8:
+
+
+# rij = 5
+# if rij <=2:
+#     print('1')
+# elif rij <= 5:
+#     print('2 ')
+# elif rij <= 8:
+#     print('3')
+
+
+x=grid(rij,colom,gok)
+print(x)
+if x == None:
+    print('is valid')
