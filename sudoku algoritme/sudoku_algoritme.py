592884e8f14b27062b6bce8c62d7d6def422b0cc
bart-van-hees <bartvanhees@student.hu.nl>
2022-06-16 13:26:12 +0200

    kleine verandering

--- sudoku algoritme/sudoku_algoritme.py
+++ sudoku algoritme/sudoku_algoritme.py
@@ -1,59 +1,55 @@
-
-test_sudoku_solved= [
-    [5,3,4,6,7,8,9,1,2],
-    [6,7,2,1,9,5,3,4,8],
-    [1,9,8,3,4,2,5,6,7],
-    [8,5,9,7,6,1,4,2,3],
-    [4,2,6,8,5,3,7,9,1],
-    [7,1,3,9,2,4,8,5,6],
-    [9,6,1,5,3,7,2,8,4],
-    [2,8,7,4,1,9,6,3,5],
-    [3,4,5,2,8,6,1,7,9],
+test_sudoku_solved = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
 ]
-
 
 test_sudoku_empty = [
-    [5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9],
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ]
-
 
 
 # z = [1,2,3,4,5,6,7,8,9]
 
 
 class recusive_backtracking():
-    def __init__(self, sudoku, rij, colom):
+    def __init__(self, sudoku):
         self.sudoku = sudoku
-        self.rij = rij
-        self.colom = colom
+        # self.rij = rij
+        # self.colom = colom
 
     def solv_sudoku(self):
-        self.rij,self.colom = self.find_zero()
-        print(self.rij,self.colom)
-        if self.rij == False and self.colom == False:
+        rij, colom = self.find_zero()
+        print(rij, colom)
+        if rij == False and colom == False:
             print("de puzzle is solved")
         else:
             for gok in range(1):
-                self.good_guess(self.sudoku,self.rij,self.colom, gok)
-
+                self.good_guess(self.sudoku, rij, colom, gok)
 
     def find_zero(self):
-        for self.rij in range(9):
-            for self.colom in range(9):
-                if self.sudoku[self.colom][self.rij] == 0:
+        for rij in range(9):
+            for colom in range(9):
+                if self.sudoku[colom][rij] == 0:
                     # self.rij = rij
                     # self.colom = colom
-                    return self.rij, self.colom
-        if(self.sudoku[self.rij][self.colom]) !=0:
-                    return False, False
+                    return rij, colom
+        if (self.sudoku[rij][colom]) != 0:
+            return False, False
 
     def good_guess(self, sudoku, rij, colom, gok):
         for item in sudoku:
@@ -63,10 +59,5 @@ class recusive_backtracking():
         print(gok)
 
 
-
-
-
-
-
-test1 = recusive_backtracking(test_sudoku_empty, 0, 0)
+test1 = recusive_backtracking(test_sudoku_empty)
 test1.solv_sudoku()
