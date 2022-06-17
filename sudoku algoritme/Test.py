23bd69f0eb3816c763ceac40b387e213a33132b7
bart-van-hees <bartvanhees@student.hu.nl>
2022-06-17 16:00:48 +0200

    paar testen in gedaan

--- sudoku algoritme/Test.py
+++ sudoku algoritme/Test.py
@@ -10,73 +10,77 @@ test_sudoku_empty = [
     [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ]
 
+for item in len(range(test_sudoku_empty)):
+    print(item)
+
+
 gok = 9
 rij = 6
 colom = 8
-def grid(rij,colom,gok):
-    if rij <= 2:
-        if colom <= 2:
-            for item in range(3):
-                for x in range(3):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
-        elif colom <= 5:
-            for item in range(3):
-                for x in range(3, 6):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
-        elif colom <= 8:
-            for item in range(3):
-                for x in range(6, 9):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
-    elif rij <= 5:
-        if colom <= 2:
-            for item in range(3,6):
-                for x in range(3):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
+# def grid(rij,colom,gok):
+#     if rij <= 2:
+#         if colom <= 2:
+#             for item in range(3):
+#                 for x in range(3):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#         elif colom <= 5:
+#             for item in range(3):
+#                 for x in range(3, 6):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#         elif colom <= 8:
+#             for item in range(3):
+#                 for x in range(6, 9):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#     elif rij <= 5:
+#         if colom <= 2:
+#             for item in range(3,6):
+#                 for x in range(3):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#
+#         elif colom <= 5:
+#             for item in range(3, 6):
+#                 for x in range(3, 6):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#
+#         elif colom <= 8:
+#             for item in range(3,6):
+#                 for x in range(6, 9):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#     elif rij <= 8:
+#         if colom<=2:
+#             for item in range(6,9):
+#                 for x in range(3):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#         elif colom <=5:
+#             for item in range(6,9):
+#                 for x in range(3,6):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#         elif colom <=8:
+#             for item in range(6,9):
+#                 for x in range(6,9):
+#                     if (test_sudoku_empty[item][x]) == gok:
+#                         return False
+#
+#
+#
+# x=grid(rij,colom,gok)
+# print(x)
+# if x != False:
+#     print('is valid')
 
-        elif colom <= 5:
-            for item in range(3, 6):
-                for x in range(3, 6):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
+x= None
+y=None
+z= None
 
-        elif colom <= 8:
-            for item in range(3,6):
-                for x in range(6, 9):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
-    elif rij <= 8:
-        if colom<=2:
-            for item in range(6,9):
-                for x in range(3):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
-        elif colom <=5:
-            for item in range(6,9):
-                for x in range(3,6):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
-        elif colom <=8:
-            for item in range(6,9):
-                for x in range(6,9):
-                    if (test_sudoku_empty[item][x]) == gok:
-                        return False
-
-
-
-# rij = 5
-# if rij <=2:
-#     print('1')
-# elif rij <= 5:
-#     print('2 ')
-# elif rij <= 8:
-#     print('3')
-
-
-x=grid(rij,colom,gok)
-print(x)
-if x == None:
-    print('is valid')
+if x ==False or y == False or z == False:
+    print('false')
+else:
+    print("true")
