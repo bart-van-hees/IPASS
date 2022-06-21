9158934533289a0aa6bfd311077229d5688d00a9
bart-van-hees <bartvanhees@student.hu.nl>
2022-06-21 15:33:17 +0200

    ben toch maar overgegaan naar tkinter

--- sudoku algoritme/GUI.py
+++ sudoku algoritme/GUI.py
@@ -1,43 +1,56 @@
-import pygame
+import tkinter as tk
 
+empty_user_grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
 
-width = 500
-height = 500
-WIN = pygame.display.set_mode((width, height))
+def add_number():
+    number = int(get_add_number.get())
+    x = int(get_x.get())
+    y = int(get_y.get())
+    get_add_number.delete(0,tk.END)
+    get_x.delete(0, tk.END)
+    get_y.delete(0, tk.END)
 
-
-pygame.display.set_caption("sudoku solver")
-
-Background_color = (255, 255, 255)
-Red = (255,0,0)
-FPS = 60
-
-
-def draw_window():
-    WIN.fill(Background_color)
-    pygame.display.update()
-    pygame.draw.rect(WIN, Red,pygame.Rect(30,30,60,60),2)
-    pygame.display.flip()
-
-
-
-
-
-def main():
-    clock = pygame.time.Clock()
-    run = True
-    while run == True:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        draw_window()
+    empty_user_grid[x-1][y-1] = number
+    for item in empty_user_grid:
+        print(item)
 
 
 
-    pygame.quit()
+window = tk.Tk(className="sudoku test raam")
+window.geometry("500x700")
+window.columnconfigure([0,1,2,3,4,5,6,7,8], minsize=50, weight=1)
+window.rowconfigure([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],minsize=50, weight=1)
+
+btn_add_number = tk.Button(master=window, text="add number", command=add_number)
+btn_add_number.grid(row = 11, column= 2,sticky="nsew")
+
+label_x = tk.Label(master=window, text="grid-x", fg="black", width=10, height=10)
+label_x.grid(row=10, column=4, sticky="nsew")
+
+label_x = tk.Label(master=window, text="grid-y", fg="black", width=10, height=10)
+label_x.grid(row=11, column=4, sticky="nsew")
+
+get_add_number = tk.Entry(master= window,width= 2, )
+get_add_number.grid(row = 10, column= 2, sticky="nsew")
+
+get_x = tk.Entry(master=window, width=2, )
+get_x.grid(row = 10, column= 5, sticky="nsew")
+
+get_y = tk.Entry(master=window, width=2, )
+get_y.grid(row = 11, column= 5, sticky="nsew")
 
 
-#zorgt ervoor dat het kan runnen 11:11
-if __name__ =="__main__":
-    main()
+
+
+
+window.mainloop()
