03b3680e52f5d043c0df79eb058b71ab37282d81
bart-van-hees <bartvanhees@student.hu.nl>
2022-06-22 13:59:55 +0200

    kleine testen gedaan

--- applicatie/testen gui.py
+++ applicatie/testen gui.py
@@ -10,11 +10,3 @@ empty_user_grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
-for rij in range(len(empty_user_grid)):
-    for getal in range(len(empty_user_grid[rij])):
-        print(rij, "is rij")
-        print(getal, "is getal")
-        print(empty_user_grid[rij][getal])
-        if rij <= 2 :
-            print('kleiner dan 2')
-            if getal <= 2:
