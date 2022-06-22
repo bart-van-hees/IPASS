cfdd78910ee6eb138f1e27ef52a8f8bd0818ed1a
bart-van-hees <bartvanhees@student.hu.nl>
2022-06-22 22:50:14 +0200

    testen in uitgevoerd

--- applicatie/testen gui.py
+++ applicatie/testen gui.py
@@ -12,13 +12,25 @@ empty_user_grid = [
     ["_", "_", "_", "_", "_", "_", "_", "_", "_"],
 ]
 
+empty_user_grid_use = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
 window = tk.Tk(className="sudoku test raam")
 window.geometry("500x800")
 window.columnconfigure([0,1,2,3,4,5,6,7,8,9], minsize=50, weight=1)
 window.rowconfigure([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],minsize=50, weight=1)
 
 lst=[]
-def generate_grid():
+def generate_input_grid():
     for rij in range(len(empty_user_grid)):
         for getal in range(len(empty_user_grid[rij])):
             if rij <= 2:
@@ -60,18 +72,22 @@ def generate_grid():
                     temp_getal = tk.Entry(master=window, relief="sunken", bg="#14213D", fg="white", justify="center")
                     temp_getal.grid(row=rij + 1, column=getal + 1, sticky="nsew")
                     lst.append(temp_getal)
-    eind_lijst=[]
-    for i in range(0, len(lst), 9):
-        eind_lijst.append(lst[i:i + 9])
 
 def get_numbers():
     eind_lijst=[]
     for i in range(0, len(lst), 9):
         eind_lijst.append(lst[i:i + 9])
 
+    for item in range(len(eind_lijst)):
+        for x in range(len(eind_lijst[item])):
+            y = eind_lijst[item][x]
+            # print(y)
+            if y.index("end") != 0:
+                print(y.get())
+                empty_user_grid[item][x] = y.get()
 
-    x=(eind_lijst[0][0])
-    print(x.get())
+    for item in empty_user_grid:
+        print(item)
     # for item in lst:
     #     x = (item.get())
     #     print(x, "met item: ", item)
