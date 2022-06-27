21d76a651945d16e93d7f33513977d3f37d33ff6
bart-van-hees <bartvanhees@student.hu.nl>
2022-06-27 21:50:20 +0200

    ideeën getest

--- algoritme/Test.py
+++ algoritme/Test.py
@@ -1,21 +1,128 @@
-test_sudoku_medium = [ #medium
-    [0, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-]
+from sudoku_algoritme import *
+import random
+
+class generate_sudoku:
+    def __init__(self):
+        self.sudoku = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        self.min_numbers = 17
 
 
-def vertical_check( colom, gok):
-    for item in test_sudoku_medium:
-        if item[colom] == gok:
+
+    def make_sudoku(self):
+        counter = 0
+        while self.min_numbers != counter:
+            x = (random.randint(0, 8))
+            y = (random.randint(0, 8))
+            gok = (random.randint(1, 9))
+            if self.good_guess(x, y, gok) != False:
+                self.sudoku[x][y] = gok
+                counter += 1
+            else:
+                continue
+
+        # valid = recusive_backtracking(self.sudoku)
+        # for item in valid.get_sudoku():
+        #     print(item)
+        for item in self.sudoku:
+            print(item)
+
+
+    def good_guess(self, rij, colom, gok):
+        y = self.horizontal_check(rij,gok)
+        x = self.grid(rij,colom, gok)
+        z = self.vertical_check(colom,gok)
+        if x == False or y == False or z == False:
             return False
         else:
-            continue
+            return True
 
-print(vertical_check(0,7))
+    def vertical_check(self, colom, gok):
+        for item in self.sudoku:
+            if item[colom] == gok:
+                return False
+            else:
+                continue
+
+    def horizontal_check(self, rij, gok):
+        for item in self.sudoku[rij]:
+            if item == gok:
+                return False
+            else:
+                continue
+
+    def grid(self, rij, colom, gok):
+        if rij <= 2:
+            if colom <= 2:
+                for item in range(3):
+                    for x in range(3):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+            elif colom <= 5:
+                for item in range(3):
+                    for x in range(3,6):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+            elif colom <= 8:
+                for item in range(3):
+                    for x in range(6,9):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+        elif rij <= 5:
+            if colom <= 2:
+                for item in range(3, 6):
+                    for x in range(3):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+            elif colom <= 5:
+                for item in range(3, 6):
+                    for x in range(3, 6):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+            elif colom <= 8:
+                for item in range(3, 6):
+                    for x in range(6, 9):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+        elif rij <= 8:
+            if colom <= 2:
+                for item in range(6, 9):
+                    for x in range(3):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+            elif colom <= 5:
+                for item in range(6, 9):
+                    for x in range(3, 6):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+            elif colom <= 8:
+                for item in range(6, 9):
+                    for x in range(6, 9):
+                        if (self.sudoku[item][x]) == gok:
+                            return False
+
+    def get_sudoku(self):
+        x = self.sudoku
+        return x
+
+
+
+
+test = generate_sudoku()
+test.make_sudoku()
+x = test.get_sudoku()
+print("\n")
+p =recusive_backtracking(x)
+p.solv_sudoku()
+for item in p.get_sudoku():
+    print(item)
+
